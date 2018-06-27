808d493ce637bd3b4cdbf002c2752eb6b7ff516f
Tim Wanders <timwanders241@gmail.com>
2018-06-27 16:51:46 +0200

    Fixed build script

--- build.sh
+++ build.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
+build() {
+	cd omnisharp-roslyn
+	sh build.sh
+}
+submodule_update() {
+	if ! command -v git > /dev/null 2>&1
+	then
+		echo "Please install 'git'"
+		exit 1
+	else
+		git submodule update --init
+	fi
+}
 
-pushd omnisharp-roslyn
-./build.sh
-popd
+if [ ! -f "omnisharp-roslyn/build.sh" ]
+then
+	submodule_update
+fi
+
+build
